5afab71d64240ea03db09976b75dd0f24e33f558
Evgeniy Vitovtov <148564043+AnyGogin31@users.noreply.github.com>
2026-03-06 16:00:28 +0300

    feat: add redis for fsm storage and docker-compose setup

--- src/configs/bot.py
+++ src/configs/bot.py
@@ -14,21 +14,20 @@
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-import asyncio
-
-from src.bot import start_bot
-from src.utils.alembic import configure_alembic
-from src.utils.logging import configure_logging
-from src.utils.uvloop import configure_uvloop
-
-
-def main():
-    configure_logging()
-    configure_uvloop()
-    configure_alembic()
-
-    asyncio.run(start_bot())
+from pydantic_settings import (
+    BaseSettings,
+    SettingsConfigDict
+)
+from pydantic import (
+    Field,
+    SecretStr
+)
 
 
-if __name__ == "__main__":
-    main()
+class BotSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_prefix="BOT_",
+        case_sensitive=False
+    )
+
+    token: SecretStr = Field(alias="BOT_TOKEN")
